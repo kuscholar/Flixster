ca8840973941f919dcab8a889034dcf13abfd153
Kusch Qin <flying-withme@hotmail.com>
2022-02-25 17:52:19 -0500

    Initial Commit

---
+++ Flixster/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Flixster
+//
+//  Created by Kusch Qin on 2/25/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
